271b4c30fa4700df6de2c44d33f754120553a6d4
matheustars2 <dias.jorge.matheus@escola.pr.gov.br>
2025-10-29 10:16:38 -0300

    e

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const avanca =document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+avanca.forEach(button =>{ 
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+
+        atual.classList. remove('ativo')
+document.getElementById(proximoPasso).classLista .add('ativo')
+    })
+
+})
